2f6642ceeb983c303673a1bc32df867f2b33013e
Larissa <peixinholarissa@outlook.com.br>
2023-07-31 21:33:05 -0300

    Hello World!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine(DateTime.Now.ToLongDateString());
-Console.WriteLine("Versão da aplicação: 1.0");
+﻿using System;
+
+namespace ControleDeVersaoComGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToLongDateString().ToUpper());
+            Console.WriteLine("Versão da aplicação: 1.0");
+
+        }
+    }
+}
